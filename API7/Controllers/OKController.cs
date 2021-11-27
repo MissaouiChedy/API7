5647b93a895dfb7ae25384b3ace628f13b45d629
Chedy Missaoui <missaoui.chedy@gmail.com>
2021-11-27 11:17:49 +0100

    First commit

---
+++ API7/Controllers/OKController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API7.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class OKController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "OK";
+        }
+    }
+}
